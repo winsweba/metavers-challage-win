6e9eda5caa0a430ebda08c7f8ce9d7164ec3b5bc
winsweba <winsweb@outlook.com>
2021-12-18 12:33:10 -0800

    login with moralis

---
+++ components/Login.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+function Login() {
+    const {authenticate} = useMoralis();
+    return (
+        <div className="bg-black relative ">
+             <h1>I am the login screen</h1>
+
+             <div className="flex flex-col absolute z-50 h-4/6 w-full 
+             items-center justify-center space-y-4">
+                 {/* wins logo  */}
+                 <Image className="object-cover rounded-full"
+                  src="https://links.papareact.com/3pi"  height={200} width={200}/>
+
+
+                 {/* Login button */}
+                 <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 animate-pulse">Login to the METAVERSE</button>
+             </div>
+
+             <div className="w-full h-screen">
+                 <Image src="https://links.papareact.com/55n" 
+                 layout="fill" 
+                 objectFit="cover"/>
+
+             </div>
+        </div>
+    )
+}
+
+export default Login
